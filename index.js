b8870ced08a68cee082f645b870f657ca1489695
ailtonconfi-lang <ailtonconfi@gmail.com>
2026-03-26 00:07:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+const { ethers } = require("ethers");
+
 console.log("🚀 Bot iniciado");
+
+// ================= RPC =================
+const RPC_URL = "https://bsc-dataseed.binance.org/";
+const provider = new ethers.JsonRpcProvider(RPC_URL);
+
+// ================= PANCAKE =================
+const ROUTER = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
+
+const routerAbi = [
+  "function getAmountsOut(uint amountIn, address[] memory path) view returns (uint[] memory amounts)"
+];
+
+const router = new ethers.Contract(ROUTER, routerAbi, provider);
+
+// ================= TOKENS =================
+const WBNB = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+const BTCB = "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c";
+const USDT = "0x55d398326f99059fF775485246999027B3197955";
 
 // ================= ESTADO =================
 let baseBTC = null;
@@ -7,19 +27,34 @@ let baseBNB = null;
 let inBTC = false;
 let inBNB = false;
 
-// ================= PREÇO (COINGECKO) =================
-async function getPrices() {
+// ================= PREÇO BTC =================
+async function getBTCPrice() {
   try {
-    const res = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,binancecoin&vs_currencies=usd");
-    const data = await res.json();
+    const amountIn = ethers.parseUnits("1", 18);
+    const path = [BTCB, WBNB, USDT];
 
-    return {
-      btc: data.bitcoin.usd,
-      bnb: data.binancecoin.usd
-    };
+    const amounts = await router.getAmountsOut(amountIn, path);
+
+    return Number(ethers.formatUnits(amounts[2], 18));
 
   } catch (err) {
-    console.log("❌ erro API:", err.message);
+    console.log("❌ BTC erro:", err.message);
+    return null;
+  }
+}
+
+// ================= PREÇO BNB =================
+async function getBNBPrice() {
+  try {
+    const amountIn = ethers.parseUnits("1", 18);
+    const path = [WBNB, USDT];
+
+    const amounts = await router.getAmountsOut(amountIn, path);
+
+    return Number(ethers.formatUnits(amounts[1], 18));
+
+  } catch (err) {
+    console.log("❌ BNB erro:", err.message);
     return null;
   }
 }
@@ -27,15 +62,13 @@ async function getPrices() {
 // ================= BOT =================
 async function runBot() {
 
-  const prices = await getPrices();
+  const btc = await getBTCPrice();
+  const bnb = await getBNBPrice();
 
-  if (!prices) {
+  if (!btc || !bnb) {
     console.log("⚠️ erro ao pegar preço");
     return;
   }
-
-  const btc = prices.btc;
-  const bnb = prices.bnb;
 
   console.log("💰 BTC:", btc);
   console.log("💰 BNB:", bnb);
